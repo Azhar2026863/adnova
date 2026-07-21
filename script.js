1c047c9945a8cd4024cf711e917f6e39da0f426c
Azhar2026863 <azharsajid671@gmail.com>
2026-07-21 19:29:06 +0500

    Create script.js

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById("users").innerHTML = "125";
+document.getElementById("balance").innerHTML = "₹50,000";
+document.getElementById("deposit").innerHTML = "8";
+document.getElementById("withdraw").innerHTML = "3";
